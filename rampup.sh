2b9c21754511681ada9455e1804cfdd50820977c
Jonathan Ellis <jbellis@datastax.com>
2023-06-21 09:08:48 -0500

    make it work on localhost

--- rampup.sh
+++ rampup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-exec screen -d -m -S 'vector_rampup' java -jar nb5.jar vector-search.yaml default.rampup \
+taskset -c 16-23 $JAVA_HOME/bin/java -jar nb5.jar vector-search.yaml default.rampup \
  errors=counter \
- driverconfig=/path/to/driver-config.json \
+ driverconfig=driver-config.json \
  dimensions=128 rampup-cycles=25M keyspace=baselines \
  -v --show-stacktraces
