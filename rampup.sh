3396fcedd04e048e9df2c918d09f486c31fbbf13
Jonathan Ellis <jbellis@datastax.com>
2023-08-05 12:37:12 -0500

    1M / 10M rampup/main

--- rampup.sh
+++ rampup.sh
@@ -3,5 +3,5 @@
 taskset -c 16-23 $JAVA_HOME/bin/java -jar nb5.jar vector-search.yaml default.rampup \
  errors=counter \
  driverconfig=driver-config.json \
- dimensions=128 rampup-cycles=25M keyspace=baselines \
+ dimensions=128 rampup-cycles=10M keyspace=baselines \
  -v --show-stacktraces
